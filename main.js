a5996158067045463a7351d972c4aeb7f7eb7a87
sasha-2002 <zahaietskij@gmail.com>
2021-03-08 18:43:53 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,32 +1,29 @@
 var map;
-var marker;
-var info;
-
-const myLatLng = { lat: 49.22619321320788, lng: 28.41249926315556 };
 
 
-info = new google.maps.InfoWindow({
-  content: '<h2>Sasha</h2>'
-});
+
 
 
 function initMap() {
-  
+  const myLatLng = { lat: 49.22619321320788, lng: 28.41249926315556 };
+  const infowindow = new google.maps.InfoWindow({
+    content: '<h2>Sasha</h2>'
+  });
   map = new google.maps.Map(document.getElementById("map"), {
     zoom: 14,
     center: myLatLng,
   });
  
   map.addListener('click', (e) => {
-    new google.maps.Marker({
+    var marker_1 = new google.maps.Marker({
       position: e.latLng,
       map: map,
       icon: "https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png"
     });
-  });
-  marker.addListener('click', function(){
+    marker_1.addListener('click', (e) => {
+      infowindow.open(map, marker_1);
+    });
     
-    info.open(map, marker);
   });
   
   
