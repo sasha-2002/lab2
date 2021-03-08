633e19eb7a2f21bb0fb706abb139c4861df1563e
sasha-2002 <zahaietskij@gmail.com>
2021-03-08 18:26:55 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,24 +1,34 @@
 var map;
+var marker;
+var info;
+
+const myLatLng = { lat: 49.22619321320788, lng: 28.41249926315556 };
+
+
+info = new google.maps.InfoWindow({
+  content: '<h2>Sasha</h2>'
+});
 
 
 function initMap() {
-  const myLatLng = { lat: 49.22619321320788, lng: 28.41249926315556 };
+  
   map = new google.maps.Map(document.getElementById("map"), {
     zoom: 14,
     center: myLatLng,
   });
-  var marker = new google.maps.Marker({
-    position: myLatLng,
-    map: map,
-    icon: "https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png"
+ 
+  map.addListener('click', (e) => {
+    new google.maps.Marker({
+      position: e.latLng,
+      map: map,
+      icon: "https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png"
+    });
   });
-
-  var info = new google.maps.InfoWindow({
-      content: '<h2>Sasha</h2>'
-  });
-
   marker.addListener('click', function(){
+    
     info.open(map, marker);
   });
+  
+  
 }
 
