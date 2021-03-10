91402533a1e2bfbe23b1d4e54baf8f02eb9c5e1d
sasha-2002 <zahaietskij@gmail.com>
2021-03-10 20:15:54 +0200

    температура

--- main.js
+++ main.js
@@ -5,12 +5,12 @@ function initMap() {
   var markers = [];
   const myLatLng = { lat: 49.22619321320788, lng: 28.41249926315556 };
 
-  var infowindow = new google.maps.InfoWindow({
-    content: '<h2>Sasha</h2>'
-  });
+  var infowindow = new google.maps.InfoWindow({ 
+    
+   });
 
   map = new google.maps.Map(document.getElementById("map"), {
-    zoom: 14,
+    zoom: 6,
     center: myLatLng,
   });
  
@@ -23,14 +23,24 @@ function initMap() {
     }));
 
     markers[markers.length-1].addListener('click', function(){
-      infowindow.setContent(this.getPosition().toString());
-      infowindow.open(map, this);  
-      foo(this.getPosition().toString());   
+      var lat = this.getPosition().lat().toString();
+      var lng = this.getPosition().lng().toString();
+      console.log(lat + "\n" + lng);
+      
+      $.getJSON(
+        "https://api.openweathermap.org/data/2.5/weather?lat="+lat+"&lon="+lng+"&appid=1b5ee5a1a74d624a74750350327ea372",
+        function(json){
+          var t = parseInt(json.main.temp-273.15);
+          console.log(t);
+          infowindow.setContent("<h2>"+
+          "t = "+
+          t.toString()+
+          "&#8451\n"+
+          "</h2>");
+          infowindow.open(map, markers[markers.length-1]);
+      });
+      
     });
     
   });
-}
-function foo(e){
-  
-  
 }
