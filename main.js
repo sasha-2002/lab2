f6836c07e80a17f1d19f25e5fc688b375f3f8e8e
sasha-2002 <zahaietskij@gmail.com>
2021-03-26 19:26:33 +0200

    change icon

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 var map;
-
+var marker;
 function initMap() {
+  marker = new google.maps.Marker();
   var markers = [];
   const myLatLng = { lat: 49.22619321320788, lng: 28.41249926315556 };
 
@@ -18,19 +19,22 @@ function initMap() {
         var t = parseInt(json.main.temp-273.15);
         infowindow.setContent(
         "<div id='txt', class='display-6'>"+
+        "<img src='http://openweathermap.org/img/wn/"+json.weather[0].icon+".png' alt='альтернативный текст'>"+
         "t = "+
         t.toString()+
-        "&#8451\n"+
+        "&#8451\n"+  
         "</div>");
-        
-        var marker = new google.maps.Marker({
+        marker.setMap(null);
+        marker = new google.maps.Marker({
           position: e.latLng,
           map: map,
           icon: {
-           url: "http://openweathermap.org/img/wn/"+json.weather[0].icon+".png"
-          }
+            path: google.maps.SymbolPath.CIRCLE,
+            scale: 0
+        }
         });
         infowindow.open(map, marker);
+        
     });
     
   });
